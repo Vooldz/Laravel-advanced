e70d0f0c87774d9f71e1c823e6f79064c78037b3
VOOLDZ <aymangh854@gmail.com>
2024-01-16 18:33:15 +0300

    Create migrations, Factories, And Seeders with some relations between them

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\FirstAdminSeeder;
+use Database\Seeders\ProductFactorySeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([FirstAdminSeeder::class]);
+        $this->call([ProductFactorySeeder::class,]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
